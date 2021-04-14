9659a27b86d74c052971dade0040152c5140644e
David Gipson <david.gipson@utexas.edu>
2021-04-14 13:08:35 -0500

    Set up Gradle project

---
+++ src/test/java/io/github/billiardballmachine/billiardballmachine/MainTest.java
@@ -0,0 +1,12 @@
+package io.github.billiardballmachine.billiardballmachine;
+
+import org.junit.jupiter.api.Test;
+
+public class MainTest {
+
+    @Test
+    public void testMainRuns() {
+        Main.main(new String[] {});
+    }
+    
+}
